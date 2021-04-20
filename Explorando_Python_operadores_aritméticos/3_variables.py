59fb5f8a3fc03eea4ac154e7cbdcf884f30c2573
medusin <jaegerfer7avendano@gmail.com>
2021-04-20 12:07:26 -0600

    aprendiendo variables

---
+++ Explorando_Python_operadores_aritméticos/3_variables.py
@@ -0,0 +1,16 @@
+a = 6
+b = 5
+print("mis variables son:")
+print("a = 6")
+print("b = 5")
+print("suma =", a + b)
+print("resta =", a - b)
+print("multiplicación =", a * b)
+
+a = b 
+print("mis variables son:")
+print("a = b")
+print("b = 5")
+print("suma =", a + b)
+print("resta =", a - b)
+print("multiplicación =", a * b)
