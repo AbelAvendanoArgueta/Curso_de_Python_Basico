e8f38e7da92e26b18886219596282ff062a253a2
medusin <jaegerfer7avendano@gmail.com>
2021-04-20 11:48:18 -0600

    Explorando Python: operadores aritméticos

---
+++ Explorando_Python_operadores_aritméticos/operadores_aritmeticos.py
@@ -0,0 +1,17 @@
+# Si no usamos la funcion print 
+# aunque python si realizara la operación
+# internamente no sera visible para el usuario
+# entonces usamos print para ver nuestras salidas
+# de datos.
+print (1 + 1)   # Asi se trabaja la suma
+print (1 - 1)   # Asi se trabajan las restas
+print (7 * 7)   # Asi se trabajan las multiplicaciones
+print (45 / 9)  # Asi se trabajan las divisiones,
+                # pero el resultado da decimales
+print (45 // 9) # si usamos doble barra inclinada no 
+                # mostrara decimales
+print(47 % 6)   # con el simbolo de porcetanjes vemos 
+                # vemos el resto de una multiplicacion 
+                # que no puede ser dividido de 
+                # forma entera
+print(2 ** 5)  # Asi se trabajan las potencias
---
+++ Explorando_Python_operadores_aritméticos/operadores_aritmeticos_raiz_cuadrada.py
@@ -0,0 +1,12 @@
+import math
+print(math.sqrt(16))
+
+# La raíz cuadrada de un número no es más que el número
+# elevado a 0,5. La función pow() en Python devuelve el 
+# valor de un número elevado a una potencia especificada
+# y se puede usar para calcular la raíz cuadrada de un 
+# número como se muestra a continuación. El primer 
+# parámetro de la función pow() es la base y el segundo 
+# es el exponente.
+
+print(pow(36,0.5))
